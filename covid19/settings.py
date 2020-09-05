e0041b3c0fa34203930407a9d2995c6f76f5dc9b
aswanthk <aswanthk0112@gmail.com>
2020-09-05 19:44:27 +0530

    files updated

--- covid19/settings.py
+++ covid19/settings.py
@@ -120,9 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'status/static')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'status/assets')
